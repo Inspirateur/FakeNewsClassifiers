53fd46b667b032d0a9c802b2feaa87bb261ad92b
Teo <teo.orthlieb@gmail.com>
2020-02-24 18:20:53 -0500

    added validation for transformer and upped the batch size to 16 (max on my gpu)

--- Logic/Classifiers/Transformer/transformer_classifier.py
+++ Logic/Classifiers/Transformer/transformer_classifier.py
@@ -14,9 +14,11 @@ class TransformerClassifier(Classifier):
 	def train(self) -> float:
 		t = text.Transformer('distilbert-base-uncased', classes=list(range(len(self.d.classes))))
 		X = self.vec.fit_transform(self.d.train.X)
+		X_val = self.vec.transform(self.d.valid.X)
 		trn = t.preprocess_train(X, self.d.train.y)
+		val = t.preprocess_test(X_val, self.d.valid.y)
 		self.model = t.get_classifier()
-		self.learner = ktrain.get_learner(self.model, train_data=trn, batch_size=8)
+		self.learner = ktrain.get_learner(self.model, train_data=trn, val_data=val, batch_size=16)
 		self.learner.fit_onecycle(lr=4e-5, epochs=2)
 
 	def predict(self, inputs: np.ndarray) -> np.ndarray:
